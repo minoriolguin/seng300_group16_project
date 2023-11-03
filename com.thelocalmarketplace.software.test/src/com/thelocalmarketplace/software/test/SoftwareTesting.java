ad41f1b9edb17c7bac1322e707c576da4089a30a
Minori Olguin <minoriiolguin@gmail.com>
2023-11-03 15:55:28 -0600

    Update .gitignore
    Update .gitignore to ignore .metadata files and .ds_store files
    
    .ds_store files are used to display items on a mac
    
    .metadata files are used for local building with eclipse

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SoftwareTesting.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SoftwareTesting.java
@@ -1,5 +1,27 @@
 package com.thelocalmarketplace.software.test;
 
-public class SoftwareTesting {
+import java.io.IOException;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thelocalmarketplace.software.SessionSimulation;
+import com.thelocalmarketplace.software.StartSession;
+
+public class SoftwareTesting {
+	
+	boolean sessionStart = false;
+	SessionSimulation session = new SessionSimulation();
+	StartSession sessionStarted = new StartSession(sessionStart);
+	
+	@Before
+	public void setup() {
+	
+	}
+	
+	@Test
+	public void sessionStartedTest() {
+		sessionStarted.setSessionStarted(true);
+		
+	}
 }
